809b2cdc324ccc3b929874666dfec3dcc254114d
Roman <roman.sav15@gmail.com>
2021-02-13 14:34:54 +0200

    fixes

--- #src/js/script.js
+++ #src/js/script.js
@@ -15,14 +15,40 @@ let front = {
             $(this).parent().parent().find(".accordion__content").slideDown(200);
             }
         });
+
+        if(window.matchMedia('(min-width: 1400px)').matches){
+          $('.has-sub-menu a').hover(
+            function(){ 
+              $(".main-screen").addClass('active');
+            },
+            function(){ 
+              $(".main-screen").removeClass('active'); 
+            }
+          );
+        }
+
+        if(window.matchMedia('(max-width: 1400px)').matches){
+          $(document).on('click', '.has-sub-menu .icon-arrow', function(){
+            
+            if ($(this).hasClass('open') ) {
+              $(this).removeClass('open');
+              $(this).next('.sub-menu').hide();
+              $(this).prev('a').removeClass('active');
+            } else {
+              $(this).addClass('open');
+              $(this).next('.sub-menu').show();
+              $(this).prev('a').addClass('active');
+            }
+          })
+        }
   },
   toggleNav: function () {
     if (!this.hamburger.hasClass('open')) {
         this.hamburger.addClass('open');
-        this.nav.toggleClass('active');
+        this.nav.slideDown('fast');
         } else {
             this.hamburger.removeClass('open');
-            this.nav.toggleClass('active');
+            this.nav.hide();
         }
     },
 
